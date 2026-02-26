40481cb49a53c733173479b404338bfeb99a031b
Jan Ptáček <honza@fogll.net>
2026-02-26 17:25:50 +0100

    Configure primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,27 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
+
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
+    provideAnimations(),
     providePrimeNG({
+      ripple: true,
       theme: {
         preset: Aura,
+        options: {
+          prefix: 'p',
+          darkModeSelector: 'light',
+          cssLayer: false,
+        },
       },
     }),
   ],
